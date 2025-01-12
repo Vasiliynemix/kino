da2b7dbf6cd09cde4b706d3448a59835ecfcef6b
Vasiliynemix <nemixvasya@gmail.com>
2025-01-12 10:32:14 +0300

    delete products getter

--- base_requests.py
+++ base_requests.py
@@ -694,13 +694,13 @@ def unblock_all(user_id, performance_id, place_id):
             if place_id == 'all':
                 # берем все брони у пользователя на этот сеанс
                 curs.execute(
-                    """SELECT performance_id, place_id, buyer_id, order_id FROM orders WHERE user_id = %s AND performance_id = %s AND status = %s;""",
+                    """SELECT performance_id, place_id, buyer_id, order_id FROM orders WHERE user_id = %s AND performance_id = %s AND status = 2;""",
                     (user_id, performance_id))
                 orders_to_close = curs.fetchall()
             else:
                 # берем все брони кроме place_id который нам передали
                 curs.execute(
-                    """SELECT performance_id, place_id, buyer_id, order_id FROM orders WHERE user_id = %s AND performance_id = %s AND status = %s AND place_id IS NOT %s;""",
+                    """SELECT performance_id, place_id, buyer_id, order_id FROM orders WHERE user_id = %s AND performance_id = %s AND status = 2 AND place_id IS NOT %s;""",
                     (user_id, performance_id, place_id))
                 orders_to_close = curs.fetchall()
 
